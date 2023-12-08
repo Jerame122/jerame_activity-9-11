cd9b6da733d4d5c0b42da7bfa9927ea02391fe36
Jerame122 <147626654+Jerame122@users.noreply.github.com>
2023-12-08 14:55:48 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     return (
         <div className="App">
             {
-              currentForm === "Login" ? <Login /> : <Register />
+              currentForm === "Login" ? <Login />
             }
         </div>
     );
