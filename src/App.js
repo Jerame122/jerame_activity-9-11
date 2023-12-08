1dc78e39a1760b5f6d754ad3aba445e2313ff464
Jerame122 <147626654+Jerame122@users.noreply.github.com>
2023-12-08 18:46:48 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import React, {useState}  from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { Login } from "./login";
+import React from 'react'
+import { Login } from './login'
 
-function App() {
-  const [currentForm, setCurrentForm]  = useState ("Login");
-
-    return (
-        <div className="App">
-            {
-              currentForm === "Login" ? <Login />
-            }
-        </div>
-    );
+const App = () => {
+  return (
+    <div>
+      <Login/>
+    </div>
+  )
 }
 
 export default App;
