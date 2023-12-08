48139557593960ddc9661dce24532aa8eac606a0
Jerame122 <147626654+Jerame122@users.noreply.github.com>
2023-12-08 14:45:39 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState}  from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Login } from "./login";
+import { Register } from "./Register";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [currentForm, setCurrentForm]  = useState ("Login");
+
+    return (
+        <div className="App">
+            {
+              currentForm === "Login" ? <Login /> : <Register />
+            }
+        </div>
+    );
 }
 
 export default App;
