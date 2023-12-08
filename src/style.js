9af9581773416e29de31c100ffb37e030a9786f0
Jerame122 <147626654+Jerame122@users.noreply.github.com>
2023-12-08 14:35:12 +0800

    Create style.js

---
+++ src/style.js
@@ -0,0 +1,11 @@
+const logInButton = {display: 'flex', justifyContent: 'center', alignItems: 'center',  margin: 'auto', marginBottom: '1rem', marginTop: '2rem'};
+const signUpButton = {display: 'flex', justifyContent: 'center', alignItems: 'center'};
+const buttonStyle = {color: 'blue'};
+const signUpStyle = {textDecoration: 'underline'};
+
+export const styles = {
+	logInButton : logInButton,
+	signUpButton : signUpButton,
+	buttonStyle : buttonStyle,
+	signUpStyle : signUpStyle
+}
